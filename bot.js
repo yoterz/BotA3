81c7cbaafdd1c0bfa7acb67c1961f6644f7c06f9
yoterz <40376255+yoterz@users.noreply.github.com>
2018-06-23 22:48:54 +0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const botconfig = require('./botconfig.json');
+
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+bot.login(botconfig.token)
+bot.on("ready",() => {
+    console.log('Ready...')
+
+})
+
+setInterval(()=>status(), 25000);
+
+bot.on("message", (msg) => {
+    if (msg.content === "aa"){
+
+      const embed = new Discord.RichEmbed()
+    .setTitle("Test github")  //หัวข้อ
+    .setAuthor("Boss Timer BDO", "https://www.picz.in.th/images/2018/06/22/48XhJt.png")  //icon หัวขอ
+
+    .setTimestamp()  //เวลาด้านล่างสุด
+     msg.channel.send({embed});
+    
+    }
+
+})
